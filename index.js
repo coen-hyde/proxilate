bca73f7ddfdd566e73a06de134651777f4b5e802
Coen Hyde <coen.hyde@gmail.com>
2015-07-06 09:28:48 -0700

    refactored some proxilate logic into middleware

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict'
 
 var httpProxy = require('http-proxy');
-var connect = require('connect');
 var basicAuth = require('basic-auth');
+var connect = require('connect');
+var mw = require('./lib/middleware');
 var _ = require('lodash');
 var url = require('url');
 
@@ -24,62 +25,11 @@ function Proxilate(options) {
 
   // Add Basic Auth if username or password is specified
   if (this.options.username || this.options.password) {
-    this.server.use(function(req, res, next) {
-      var credentials = basicAuth(req);
-
-      // Return unauthorized if no credentials or invalid credentials
-      var unauthorized = (
-        !credentials ||
-        credentials.name !== options.username ||
-        credentials.pass !== options.password
-      );
-
-      if (unauthorized) {
-        res.statusCode = 401;
-        return res.end();
-      }
-
-      // Lets proxy
-      next();
-    });
+    this.server.use(mw.auth(this.options.username, this.options.password));
   }
 
-  // Setup proxy
-  var proxy = new httpProxy.createProxyServer();
-
-  // Listen for the `error` event on `proxy`.
-  proxy.on('error', function (err, req, res) {
-    res.writeHead(500, {'Content-Type': 'text/plain'});
-    res.end('Something went wrong. And we are reporting a custom error message.');
-  });
-
-  // Middleware to perform the redirect
-  this.server.use(function (req, res, next) {
-    var forwardUrl = req.originalUrl.substr(1);
-    var forwardUrlInfo = url.parse(forwardUrl);
-
-    // URL to forward to must be absolute.
-    if (!forwardUrlInfo.protocol || !forwardUrlInfo.hostname) {
-      res.writeHead(400, { 'Content-Type': 'text/plain' });
-      res.write('forward url must be absolute');
-      return res.end();
-    }
-
-    // var forwardPath = forwardUrl.substring(forwardUrl.indexOf(forwardUrlInfo.path));
-    req.url = forwardUrlInfo.path;
-
-    var forwardHost = forwardUrlInfo.protocol+"//";
-    if (forwardUrlInfo.auth) {
-      forwardHost += forwardUrlInfo.auth+'@';
-    }
-    forwardHost += forwardUrlInfo.host;
-
-    // Proxy request
-    proxy.web(req, res, {
-      changeOrigin: true,
-      target: forwardHost
-    });
-  });
+  // Add Proxy Middleware
+  this.server.use(mw.proxy());
 }
 
 /*
