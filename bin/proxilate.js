879642650257305d6b6f267383bdd2ceb279cf0e
Coen Hyde <coen.hyde@gmail.com>
2015-07-06 09:31:13 -0700

    added ability to specify basic auth credentials via proxilate cli

--- bin/proxilate.js
+++ bin/proxilate.js
@@ -1,12 +1,23 @@
 #!/usr/bin/env node
 var proxilate = require('../');
+var _ = require('lodash');
 
 var argv = require('yargs')
-    .usage('Usage: $0 -p [port]')
-    .default('p', 9235);
+    .usage('Usage: $0 -p [port] --username=[username] --password=[password]')
+    .describe('username', 'BasicAuth username')
+    .describe('password', 'BasicAuth password')
+    .help('h')
+    .alias('h', 'help')
+    .option('p', {
+      alias: 'port',
+      describe: 'port',
+      default: 9235
+    })
+    .version(function() {
+      return require('../package').version;
+    })
+    .argv;
 
-var proxy = proxilate({
-  port: argv.p
-})
-
+var options = _.pick(argv, ['port', 'username', 'password']);
+var proxy = proxilate(options);
 proxy.start();
